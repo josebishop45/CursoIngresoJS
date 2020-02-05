02c515af5d3279423bbfac75bfeeebe33667f68c
alumno <alumno>
2020-02-05 10:13:50 -0300

    programacion

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,37 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
+
+    var nombre;
+
+    var edad;
+
+  nombre = document.getElementById("elNombre").value;
+
+  edad = document.getElementById("laEdad").value;
+
+  // console.log(nombre, edad);
+
+
+  alert("usted se llama "  + nombre + " y tiene " + edad +  " años");
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
 	
 }
 
